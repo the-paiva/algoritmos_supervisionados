561346be3053e3026e9496bfe1eb27a1f7ca174e
Henrique Paiva <hrpaiva3@gmail.com>
2026-06-04 13:52:30 -0300

    Adiciona a função treinar_k

--- modulos.py
+++ modulos.py
@@ -1,4 +1,11 @@
+# ================================== IMPORTS ==================================
+
+
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# ================================== FUNÇÕES GERAIS ==================================
 
 
 # Avalia um modelo de acordo com as métricas exigidas
@@ -31,11 +38,6 @@ def exibir_relatorio_de_classificacao(y_test, y_pred, iris):
     print("=" * 69)
 
 
-# Ajuste de valores iniciais necessários para as análises com Iris
-def preparar_iris(iris):
-    return iris.data, iris.target, iris.feature_names, iris.target_names
-
-
 # Verifica se o valor atual de k tem um desempenho maior do que o melhor valor de k
 # até então, e o atualiza caso isso aconteça.
 def atualizar_melhor_desempenho_k(f1_knn, k, melhor_f1, melhor_k):
@@ -50,3 +52,24 @@ def exibir_melhor_resultado_do_knn(melhor_k, melhor_f1):
     print("=" * 50)
     print(f"CONCLUSÃO: O melhor desempenho foi com K = {melhor_k} (F1-Score: {melhor_f1:.4f})")
     print("=" * 50)
+
+
+# Realiza o treinamento e avaliação de um modelo que usa KNN de acordo com o valor escolhido de K
+def treinar_k(k, resultados_acuracia, X_train, y_train, X_test, y_test):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred_knn = knn.predict(X_test)
+    acuracia_knn, precisao_knn, revocacao_knn, f1_knn = avaliar_modelo(y_test, y_pred_knn)
+
+    # Guardamos a acurácia para plotar o gráfico depois
+    resultados_acuracia.append(acuracia_knn)
+
+    return acuracia_knn, precisao_knn, revocacao_knn, f1_knn
+
+
+# ================================== FUNÇÕES DO IRIS ==================================
+
+
+# Ajuste de valores iniciais necessários para as análises com Iris
+def preparar_iris(iris):
+    return iris.data, iris.target, iris.feature_names, iris.target_names
