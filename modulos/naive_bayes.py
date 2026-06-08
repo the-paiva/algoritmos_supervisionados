65c4480220c2e65f7dd096d8ad2e1d6dda089e98
Henrique Paiva <hrpaiva3@gmail.com>
2026-06-07 23:11:56 -0300

    Adiciona a função executar_naive_bayes_multinomial

--- modulos/naive_bayes.py
+++ modulos/naive_bayes.py
@@ -3,7 +3,7 @@
 
 # Imports necessários
 from modulos.gerais import avaliar_modelo
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 
 
 # Realiza todo o processo de preparação e avaliação de um modelo com Naive Bayes
@@ -21,3 +21,18 @@ def executar_naive_bayes(X_train, y_train, X_test, y_test):
 
     return y_pred_gnb, acuracia_gaussian, precisao_gaussian, revocacao_gaussian, f1_gaussian
 
+
+# Realiza todo o processo de preparação e avaliação de um modelo com Naive Bayes Multinomial
+def executar_naive_bayes_multinomial(X_train, y_train, X_test, y_test):
+    # Aplicação do algoritmo Naive Bayes Multinomial
+    multinomial = MultinomialNB()
+    multinomial.fit(X_train, y_train)
+
+    # Predição
+    y_pred_multinomial = multinomial.predict(X_test)
+
+    # Avaliação das métricas usando Naive Bayes com Breast Cancer
+    (acuracia_multinomial, precisao_multinomial,
+    revocacao_multinomial, f1_multinomial) = avaliar_modelo(y_test, y_pred_multinomial)
+
+    return y_pred_multinomial, acuracia_multinomial, precisao_multinomial, revocacao_multinomial, f1_multinomial
